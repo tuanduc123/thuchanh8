33b53d8276c1beb24e545022646c905ea0b2802c
tuanduc123 <48420332+tuanduc123@users.noreply.github.com>
2019-05-30 02:13:03 +0700

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+tk = Tk()
+
+tk.title("THÔNG TIN SINH VIÊN")
+
+Label(tk, text = "Họ tên: Lê Hoài Nam").pack()
+Label(tk, text = "Ngày sinh: 04/09/1999").pack()
+Label(tk, text = "MSSV: 1755252021600007").pack()
+Label(tk, text = "Ngành học: Kỹ thuật ĐK&TĐH").pack()
+
+tk.mainloop()
